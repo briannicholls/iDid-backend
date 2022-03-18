0aa44cc75e798e4e3c97b90d77381776bf3f1191
Brian <nichol88@gmail.com>
2022-03-18 17:49:14 -0400

    create invitations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_26_080121) do
+ActiveRecord::Schema.define(version: 2022_03_18_023223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 2020_07_26_080121) do
     t.string "measurement_unit"
   end
 
+  create_table "invitations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "friend_id", null: false
+    t.boolean "confirmed"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_invitations_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "fname"
     t.string "lname"
@@ -50,4 +59,5 @@ ActiveRecord::Schema.define(version: 2020_07_26_080121) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "invitations", "users"
 end
