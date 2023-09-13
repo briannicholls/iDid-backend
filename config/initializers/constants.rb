8666cbf5454db516b810d26434a510d97ed92460
Brian Nicholls <nichol88@gmail.com>
2023-09-12 20:23:56 -0400

    update rails credentials for production

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -4,6 +4,7 @@ module Constants
   GMAIL_USERNAME    = Rails.application.credentials[:gmail_username]
   GMAIL_PASSWORD    = Rails.application.credentials[:gmail_password]
   CORS_ORIGINS      = Rails.application.credentials[:cors_origins].split(',').map(&:strip)
+  # For development only; Heroku uses URL for production
   DATABASE_USERNAME = Rails.application.credentials[:database_username]
   DATABASE_PASSWORD = Rails.application.credentials[:database_password]
 end
