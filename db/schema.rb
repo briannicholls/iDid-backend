2933feddb7b88c7f84affa3dfd6b129194d66574
Brian Nicholls <nichol88@gmail.com>
2023-10-21 15:57:28 -0400

    add track_reps to counters

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_21_183436) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_21_194516) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_21_183436) do
     t.string "dimension", default: "default"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "track_reps", default: false
   end
 
   create_table "invitations", force: :cascade do |t|
