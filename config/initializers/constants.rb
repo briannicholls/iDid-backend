8f84cef52997bdb5e8c598425675bc614a84eb68
Brian Nicholls <nichol88@gmail.com>
2024-02-25 16:38:57 -0500

    use safe navigator for ENV var

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -2,7 +2,7 @@
 
 module Constants
   FRONTEND_BASE_URL = ENV['FRONTEND_BASE_URL']
-  CORS_ORIGINS      = ENV['CORS_ORIGINS'].split(',').map(&:strip)
+  CORS_ORIGINS      = ENV['CORS_ORIGINS']&.split(',')&.map(&:strip)
 
   # For development only; Heroku uses URL for production
   DATABASE_USERNAME = ENV['DATABASE_USERNAME']
